c4bc15a8e805bc369d2f798b2c9c1fbdd7a5f113
Ubuntu <ubuntu@ip-172-31-18-94.eu-west-1.compute.internal>
2019-11-12 10:37:26 +0000

    fix

--- js/config.js
+++ js/config.js
@@ -1,9 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_PFuloVFwV',
+        userPoolId: 'eu-west-1_PFuloVFwV', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '75ud00b5qje3u7924c07h636oq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
-	    invokeUrl: 'https://b5cll4ud52.execute-api.eu-west-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://b5cll4ud52.execute-api.eu-west-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
 };
