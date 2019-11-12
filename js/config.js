9cb684a7868c7e3c8f42ebe54b2c9f7d5bd2c6c6
Ubuntu <ubuntu@ip-172-31-18-94.eu-west-1.compute.internal>
2019-11-12 10:28:31 +0000

    fix

--- js/config.js
+++ js/config.js
@@ -5,5 +5,5 @@ window._config = {
         region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+	    invokeUrl: 'https://b5cll4ud52.execute-api.eu-west-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
 };
